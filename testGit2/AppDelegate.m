bc2f4af667812a332166961a4c6f139672877266
Thidaporn Kijkamjai <jinglejill@me.com>
2018-06-04 01:19:31 +0700

    test git

--- testGit2/AppDelegate.m
+++ testGit2/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    NSLog(@"test git 2");
     return YES;
 }
 
